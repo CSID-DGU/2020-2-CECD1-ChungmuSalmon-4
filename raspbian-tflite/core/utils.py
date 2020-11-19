40082037a4e05fea5fbf16fc1df32f47548755cb
Junyoung <tom9744@gmail.com>
2020-11-20 00:51:20 +0900

    Added Object Counting & HTTP Reqeust functions

--- raspbian-tflite/core/utils.py
+++ raspbian-tflite/core/utils.py
@@ -2,6 +2,8 @@ import cv2
 import random
 import colorsys
 import numpy as np
+import requests
+import datetime
 
 from core.config import cfg
 
@@ -14,15 +16,54 @@ def read_class_names(class_file_name):
     return names
 
 
+def calc_object_number(bboxes):
+    class_names = read_class_names(cfg.YOLO.CLASSES)
+    counts = dict()
+
+    out_boxes, out_scores, out_classes, num_boxes = bboxes
+    for i in range(num_boxes[0]):
+        """ Count the number of objects """
+
+        class_index = int(out_classes[0][i])
+        class_name = class_names[class_index]
+        counts[class_name] = counts.get(class_name, 0) + 1
+
+    return counts
+
+
+def sent_to_server(counts):
+    api_url = "http://localhost:3000/counts"
+    now = datetime.datetime.now()
+
+    person_count = counts.get('person') if counts.get('person') is not None else 0
+    tent_count = counts.get('tent') if counts.get('tent') is not None else 0
+
+    new_counting_result = {
+        "time": now.strftime('%Y-%m-%d %H:%M:%S'),
+        "location": "CampingZone",
+        "person": str(person_count),
+        "tents": str(tent_count)
+    }
+
+    print(new_counting_result)
+
+    response = requests.post(
+        url=api_url,
+        data=new_counting_result
+    )
+
+    print(response)
+    print(response.text)
+
+    return True
+
+
 def draw_bbox(image, bboxes, classes=read_class_names(cfg.YOLO.CLASSES), show_label=True):
     num_classes = len(classes)
     image_h, image_w, _ = image.shape
     hsv_tuples = [(1.0 * x / num_classes, 1., 1.) for x in range(num_classes)]
     colors = list(map(lambda x: colorsys.hsv_to_rgb(*x), hsv_tuples))
     colors = list(map(lambda x: (int(x[0] * 255), int(x[1] * 255), int(x[2] * 255)), colors))
-
-    class_names = read_class_names(cfg.YOLO.CLASSES)
-    counts = dict()
 
     random.seed(0)
     random.shuffle(colors)
@@ -36,12 +77,6 @@ def draw_bbox(image, bboxes, classes=read_class_names(cfg.YOLO.CLASSES), show_la
         coor[2] = int(coor[2] * image_h)
         coor[1] = int(coor[1] * image_w)
         coor[3] = int(coor[3] * image_w)
-
-        """ Count the number of objects """
-
-        class_index = int(out_classes[0][i])
-        class_name = class_names[class_index]
-        counts[class_name] = counts.get(class_name, 0) + 1
 
         fontScale = 0.5
         score = out_scores[0][i]
@@ -59,7 +94,5 @@ def draw_bbox(image, bboxes, classes=read_class_names(cfg.YOLO.CLASSES), show_la
 
             cv2.putText(image, bbox_mess, (c1[0], np.float32(c1[1] - 2)), cv2.FONT_HERSHEY_SIMPLEX,
                         fontScale, (0, 0, 0), bbox_thick // 2, lineType=cv2.LINE_AA)
-
-    print(counts)
 
     return image
